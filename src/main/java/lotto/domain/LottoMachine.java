748a36d36d160e79fb5d691976cdc5504c8f5cd4
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2022-12-16 16:22:28 +0900

    feat(lotto) : 금액에 맞는 로또를 구입하는 기능 추가

---
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -0,0 +1,30 @@
+package lotto.domain;
+
+import lotto.Lotto;
+import lotto.util.NumberGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    private static final int LOTTO_PRICE = 1000;
+    private final NumberGenerator generator;
+
+    public LottoMachine(NumberGenerator generator) {
+        this.generator = generator;
+    }
+
+    public List<Lotto> buy(int amount) {
+        List<Lotto> lottos = new ArrayList<>();
+        int count = numberOfLotto(amount);
+        for (int i = 0; i < count; i++) {
+            Lotto lotto = new Lotto(generator.generate());
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
+
+    private int numberOfLotto(int amount) {
+        return amount % LOTTO_PRICE;
+    }
+}
