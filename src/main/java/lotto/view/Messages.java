abb0964e6c0e37419d162bce8918df39a3f8dc31
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2022-12-16 20:56:35 +0900

    feat(lotto) : 통계를 출력

--- src/main/java/lotto/view/Messages.java
+++ src/main/java/lotto/view/Messages.java
@@ -4,7 +4,10 @@ public enum Messages {
     INPUT_AMOUNT("구입금액을 입력해 주세요."),
     NUMBER_OF_LOTTO("개를 구매했습니다."),
     INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
-    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    STATISTICS("당첨 통계\n" +
+            "---"),
+    WIN_INFO("%d개 일치 (%d)원 - %d개\n");
 
     private final String message;
 
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,16 +1,20 @@
 package lotto.view;
 
 import lotto.Lotto;
+import lotto.domain.Rank;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
     public static void inputAmount() {
-        System.out.println(Messages.INPUT_AMOUNT);
+        System.out.println(Messages.INPUT_AMOUNT.getMessage());
     }
 
     public static void numberOfLotto(int number) {
-        System.out.println(number + Messages.NUMBER_OF_LOTTO.toString());
+        System.out.println(number + Messages.NUMBER_OF_LOTTO.getMessage());
     }
 
     public static void lottoNumbers(List<Lotto> lottos) {
@@ -18,12 +22,23 @@ public class OutputView {
     }
 
     public static void inputWinningNumbers() {
-        System.out.println(Messages.INPUT_WINNING_NUMBERS);
+        System.out.println(Messages.INPUT_WINNING_NUMBERS.getMessage());
     }
 
     public static void inputBonusNumber() {
-        System.out.println(Messages.INPUT_BONUS_NUMBER);
+        System.out.println(Messages.INPUT_BONUS_NUMBER.getMessage());
     }
 
-
+    public static void statistics(Map<Rank, Integer> result) {
+        System.out.println(Messages.STATISTICS.getMessage());
+        Arrays.stream(Rank.values())
+                .filter(rank -> rank != Rank.MISS)
+                .sorted(Comparator.reverseOrder())
+                .forEach(rank -> System.out.printf(
+                        Messages.WIN_INFO.getMessage(),
+                        rank.getCountOfMatch(),
+                        rank.getWinningMoney(),
+                        result.get(rank))
+                );
+    }
 }
